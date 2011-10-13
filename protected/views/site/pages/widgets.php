d72968a2aa96950575b02816933fe943ec0cddf3
Tobias Munk <schmunk@usrbin.de>
2011-10-13 22:31:15 +0200

    cleanup

--- protected/views/site/pages/widgets.php
+++ protected/views/site/pages/widgets.php
@@ -10,17 +10,13 @@
 
 <p>For more details on how to further develop this application, please visit <a href="http://www.phundament.com/">our website</a>.</p>
 
-<div class="span-22 last">
-	<?php $this->widget('p3widgets.components.P3WidgetContainer', array('id' => 'main1')) ?>
+<div class="span-23 last">
+	<?php $this->widget('p3widgets.components.P3WidgetContainer', array('id' => 'main1', 'varyByRequestParam' => 'view')) ?>
 </div>
 <div style="height: 20px; clear: both;"></div>
-<div class="span-11">
-	<?php $this->widget('p3widgets.components.P3WidgetContainer', array('id' => 'main2')) ?>
+<div class="span-11 append-1">
+	<?php $this->widget('p3widgets.components.P3WidgetContainer', array('id' => 'main2', 'varyByRequestParam' => 'view')) ?>
 </div>
 <div class="span-11 last">
-	<?php $this->widget('p3widgets.components.P3WidgetContainer', array('id' => 'main3')) ?>
-</div>
-<div style="height: 20px; clear: both;"></div>
-<div class="span-16">
-	<?php $this->widget('p3widgets.components.P3WidgetContainer', array('id' => 'main4')) ?>
+	<?php $this->widget('p3widgets.components.P3WidgetContainer', array('id' => 'main3', 'varyByRequestParam' => 'view')) ?>
 </div>
