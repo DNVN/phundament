cf346fbda817b5918f313a8a59d6169c53c1e489
Tobias Munk <schmunk@usrbin.de>
2011-11-01 19:48:12 +0100

    fixed layout

--- protected/views/wiki/index.php
+++ protected/views/wiki/index.php
@@ -6,12 +6,12 @@ $this->breadcrumbs = array(
 ?>
 	
 
-<div class="span-6">
+<div class="span-4 border">
 <h2>Navigation</h2>
 <?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>$items)); ?>
 </div>
-<div class="span-18 last">
+<div class="span-20 last">
 <h1><?php echo $this->id . '/' . $this->action->id.((isset($_GET['page']))?'/'.$_GET['page']:''); ?></h1>
 <?php $this->widget('p3widgets.components.P3WidgetContainer', array('id' => 'main', 'varyByRequestParam' => 'page')) ?>
 </div>
