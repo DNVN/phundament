eae107ba6fda2f5163485bf251f6409f54e516ce
Tobias Munk <schmunk@usrbin.de>
2011-11-09 15:18:48 +0100

    updated demo wiki controller, cleanup

--- protected/views/wiki/index.php
+++ protected/views/wiki/index.php
@@ -1,19 +1,23 @@
 <?php
+$page = ((isset($_GET['page'])) ? $_GET['page'] : '');
 $this->layout = "//layouts/main";
 $this->breadcrumbs = array(
-	'Wiki',
+	'Wiki' => array('/wiki'),
+	$page
 );
 ?>
-	
 
-<div class="span-4 border">
-<h2>Navigation</h2>
-<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>$items)); ?>
+
+<div class="span-24 last">
+	<h1><?php echo $this->id . '/' . $page; ?></h1>
+	<div class="wiki-navigation">
+		<?php $this->widget('zii.widgets.CMenu', array(
+			'items' => $items)); ?>
+	</div>
 </div>
-<div class="span-20 last">
-<h1><?php echo $this->id . '/' . $this->action->id.((isset($_GET['page']))?'/'.$_GET['page']:''); ?></h1>
-<?php $this->widget('p3widgets.components.P3WidgetContainer', array('id' => 'main', 'varyByRequestParam' => 'page')) ?>
+
+<div class="span-24 last">
+	<?php $this->widget('p3widgets.components.P3WidgetContainer', array('id' => 'main', 'varyByRequestParam' => 'page')) ?>
 </div>
 
 <div class="clearfix"></div>
