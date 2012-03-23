a3b0e17e1b523c25aa58cf32c5dd7c783c4608ed
Tobias Munk <schmunk@usrbin.de>
2012-03-23 01:16:12 +0100

    refactoring part 1 of ? - moving modules and themes into extension

---
+++ protected/config/p3console.php
@@ -0,0 +1,30 @@
+<?php
+
+// This is the configuration for yiic console application.
+// Any writable CConsoleApplication properties can be configured here.
+$mainConfig = require(dirname(__FILE__).DIRECTORY_SEPARATOR.'main.php');
+
+return array(
+	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+	'name'=>'My Console Application',
+	// application components
+	'components'=>array(
+		'db'=>$mainConfig['components']['db']
+	),
+    'commandMap' => array(
+        'rsync'=>array(
+            'class' => 'ext.p3extensions.commands.P3RsyncCommand',
+            'servers' => array(
+                'dev' => realpath(dirname(__FILE__).'/..'),
+                'prod' => 'user@example.com:/path/to/webapp',
+            ),
+            'aliases' => array(
+                'data' => 'application.data'
+            ),
+        ),
+        'dumpschema'=>array(
+            'class' => 'ext.p3extensions.commands.P3DumpSchemaCommand',
+		),
+    ),
+#	'import' => $mainConfig['import']
+);
